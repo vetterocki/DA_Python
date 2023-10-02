b63aef638f0e668d8b220ca9e4b87bed62a974e5
vetterocki <vitbutriy@gmail.com>
2023-10-02 15:46:38 +0300

    lab2 init commit

--- lab1/dict_utils.py
+++
@@ -1,6 +0,0 @@
-def create(function_locals):
-    return {var.capitalize(): value for var, value in function_locals.items()}
-
-
-def joined(some_dict):
-    return "\n".join([f"{k}:\n{v}\n" for k, v in some_dict.items()])
---
+++ lab2/main.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+from dict_utils import joined
+
+
+def find_mean(columns, csv_data):
+    return list(map(lambda column: np.mean(csv_data[column]), columns))
+
+
+def check_if_normal(columns, csv_data):
+    return list(map(lambda column: stats.normaltest(csv_data[column]).pvalue, columns))
+
+
+def check_if_crime_rate_increased(columns, csv_data):
+    return stats.ttest_ind(*[csv_data[column] for column in columns]).pvalue
+
+
+def calculate_correlation(columns, csv_data):
+    result = {}
+    confidence_level = 0.05
+    for i in range(0, len(columns), 2):
+        first, second = columns[i], columns[i + 1]
+        is_normal = any(p < confidence_level for p in check_if_normal([first, second], csv_data))
+        correlation_func = stats.spearmanr if is_normal else stats.pearsonr
+        result[correlation_func(csv_data[first], csv_data[second])] = "Spearman" if is_normal else "Pearson"
+    return result
+
+
+data = pd.read_csv("lab2\Crime.csv")
+crime_rates_columns = ["CrimeRate", "CrimeRate10"]
+
+print(f"""
+1.Знайти середню частоту злочинів (зараз і десять років тому): 
+{find_mean(crime_rates_columns, data)}
+2.Перевірити чи нормально розподілена частота злочинів (зараз і десять років тому):
+p_values, {check_if_normal(crime_rates_columns, data)}
+3.Перевірити за допомогою статистичних гіпотез чи зросла частота злочинів за 10 років:
+p_value, {check_if_crime_rate_increased(crime_rates_columns, data)}
+4.Який зв’язок між частотою злочинів і витратами на поліцію (коефіцієнт Спірмена): \n
+{joined(calculate_correlation(["CrimeRate", "ExpenditureYear0", "CrimeRate10", "ExpenditureYear10"], data))}
+""")
